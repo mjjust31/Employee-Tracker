fd10e390273ac5a9d77cbda8e6456b0ff5cde2d7
justmann <mj.justmann@gmail.com>
2023-10-22 18:02:00 -0500

    setting-up-mysql

---
+++ script/prompt.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "userSelect",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Quit",
+      ],
+      default: "View All Employees",
+    },
+  ]);
+}
+
+module.exports = promptUser; 
---
+++ server.js
@@ -0,0 +1,24 @@
+const promptUser = require("./script/prompt");
+const fs = require("fs");
+const path = require("path");
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password here
+    password: "Password1",
+    database: "movies_db",
+  },
+  console.log(`Connected to the movies_db database.`)
+);
+
+promptUser().then((answers) => {
+  console.log(answers);
+});
