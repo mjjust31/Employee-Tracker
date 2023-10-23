c89ef632765035d9d9ee27fefd155f90653ac94b
justmann <mj.justmann@gmail.com>
2023-10-23 12:11:35 -0500

    added-manager-table

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,33 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
-
-
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
-
-
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary INT NOT NULL,
   manager BOOLEAN,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
-
-
 CREATE TABLE managers (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   full_Name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
+  manager BOOLEAN,
+  department_id INT NOT NULL,
   FOREIGN KEY (role_id) REFERENCES roles(id)
-  ) 
-  
-  
-  CREATE TABLE employees (
+);
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+  department_id INT NOT NULL,
+  salary INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES managers(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,27 +3,30 @@ VALUES ('Engineering'),
   ('Finance'),
   ('Legal'),
   ('Sales');
-
-  
-INSERT INTO roles (title, department_id, salary, manager)
-VALUES ('Sales Lead', 4, 100000, true),
-  ('Salesperson', 4, 100000, false),
-  ('Lead Engineer', 1, 150000, true),
-  ('Software Engineer', 1, 120000, false),
-  ('Account Manger', 2, 160000, true),
-  ('Accountant', 2, 125000, false),
-  ('Legal Team Lead', 3, 250000, true),
-  ('Laywer', 3, 190000, false);
+INSERT INTO roles (title, department_id, manager)
+VALUES ('Sales Lead', 4, true),
+  ('Salesperson', 4, false),
+  ('Lead Engineer', 1,  true),
+  ('Software Engineer', 1, false),
+  ('Account Manger', 2, true),
+  ('Accountant', 2, false),
+  ('Legal Team Lead', 3, true),
+  ('Laywer', 3, false);
 
 
+INSERT INTO managers (full_Name, role_id, manager, department_id)
+VALUES ('John Doe', 2, 1, 4),
+  ('Ashley Rodriguez', 3, 1, 1),
+  ('Kunal Singh', 6, 1, 2),
+  ('Sara Lourd', 8, 1, 4);
 
 
--- INSERT INTO employees (first_name, last_name, role_id, department_id, salary, manager)
--- VALUES ('John', 'Doe', 1, 4),
---   ('Mike', 'Chan', 2, 4),
---   ('Ashley', 'Rodriguez', 3, 1),
---   ('Kevin', 'Tupik', 4, 1),
---   ('Kunal', 'Singh', 5, 2),
---   ('Malia', 'Brown', 6, 2),
---   ('Sara', 'Lourd', 7, 3),
---   ('Tom', 'Allen', 8, 3);
+INSERT INTO employees (first_name, last_name, role_id, department_id, salary, manager_id)
+VALUES ('John', 'Doe', 1, 4, 100000, 1),
+  ('Mike', 'Chan', 2, 4, 800000, NULL),
+  ('Ashley', 'Rodriguez', 3, 1, 150000, 1),
+  ('Kevin', 'Tupik', 4, 1, 120000, NULL),
+  ('Kunal', 'Singh', 5, 2, 160000, 1),
+  ('Malia', 'Brown', 6, 2, 125000, NULL ),
+  ('Sara', 'Lourd', 7, 3, 250000, 1),
+  ('Tom', 'Allen', 8, 3, 190000, NULL);
