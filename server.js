d0f1f444d8bf1ca29f2fad9ce48b9362bdc6795e
justmann <mj.justmann@gmail.com>
2023-11-10 19:11:17 -0600

    add-employee

--- server.js
+++ server.js
@@ -278,59 +278,74 @@ function addEmployee() {
     console.table(titles);
 
     db.query(`SELECT * FROM departments`, function (err, departments) {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is the employee's first name?",
-            name: "firstName",
-            validate: function (input) {
-              const done = this.async();
-              if (input.length === 0) {
-                done("You need to enter a first name.");
-                return;
-              }
-              done(null, true);
+      // Fetch managers data from the database
+      db.query(`SELECT * FROM managers`, function (err, managers) {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "What is the employee's first name?",
+              name: "firstName",
+              validate: function (input) {
+                const done = this.async();
+                if (input.length === 0) {
+                  done("You need to enter a first name.");
+                  return;
+                }
+                done(null, true);
+              },
             },
-          },
-          {
-            type: "input",
-            message: "What is the employee's last name?",
-            name: "lastName",
-            validate: function (input) {
-              const done = this.async();
-              if (input.length === 0) {
-                done("You need to enter a last name.");
-                return;
-              }
-              done(null, true);
+            {
+              type: "input",
+              message: "What is the employee's last name?",
+              name: "lastName",
+              validate: function (input) {
+                const done = this.async();
+                if (input.length === 0) {
+                  done("You need to enter a last name.");
+                  return;
+                }
+                done(null, true);
+              },
             },
-          },
-          {
-            type: "list",
-            message: "Please select the employee's title",
-            name: "employeeTitle",
-            choices: titles.map((title) => title.title),
-          },
-        ])
-        .then((answers) => {
-          const { firstName, lastName, employeeTitle } = answers;
-          console.log(employeeTitle);
+            {
+              type: "list",
+              message: "Please select the employee's title",
+              name: "employeeTitle",
+              choices: titles.map((title) => title.title),
+            },
+          ])
+          .then((answers) => {
+            const { firstName, lastName, employeeTitle } = answers;
 
-          const selectedTitle = titles.find(
-            (title) => title.title === employeeTitle
-          );
-          console.log(selectedTitle);
-          const titleId = selectedTitle.id;
-          console.log(titleId);
+            const selectedTitle = titles.find(
+              (title) => title.title === employeeTitle
+            );
+            console.log(selectedTitle);
+            const titleId = selectedTitle.id;
+            console.log(titleId);
 
-          const selectedDepartment = departments.find(
-            (department) => department.id === selectedTitle.department_name
-          );
-          console.log(selectedDepartment);
-          const departmentId = selectedDepartment.id; 
-          console.log(departmentId)
-        });
+            const selectedDepartment = departments.find((department) => department.department_name === selectedTitle.department_name);
+            console.log(selectedDepartment);
+            const departmentId = selectedDepartment.id;
+            console.log(departmentId);
+            // const selectedManager = managers.find(
+            //   (manager) => manager.title === titleId
+            // );
+            // console.log(selectedManager);
+            // const managerId = selectedManager.id;
+            // console.log(managerId);
+
+            // const newEmployee = {
+            //   first_name: firstName,
+            //   last_name: lastName,
+            //   title: titleId,
+            //   manager_name: managerId
+            // }
+
+            // console.log(newEmployee)
+          });
+      });
     });
   });
 }
